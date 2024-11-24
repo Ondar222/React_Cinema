f35ae9961ccf0754edc6a8f7d5d71bc57bc4821d
Furigane <freedominprogramming@gmail.com>
2024-11-24 18:46:49 +0300

    Layout transfer

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <>
+            <p>Hello World</p>
+            <p>I`m Front - Dev</p>
+            <p>Hello World</p>
+            <p>I`m Front - Dev</p>
+            <p>Hello World</p>
+            <p>I`m Front - Dev</p>
+        </>
+    )
+}
+
+export default Header;
